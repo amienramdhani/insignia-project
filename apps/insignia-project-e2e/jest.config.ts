8acd1c8768739e35dd656c37a546598f65da35be
amienramdhani <mar081201@gmail.com>
2024-09-22 11:43:44 +0700

    Initial commit

---
+++ apps/insignia-project-e2e/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  displayName: 'insignia-project-e2e',
+  preset: '../../jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/insignia-project-e2e',
+};
